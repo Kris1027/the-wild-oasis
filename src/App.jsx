7b8bdab0fe1ca5f79ca9db80d98729d7177deda3
Kris1027 <obarzanek.work@gmail.com>
2023-09-24 19:24:38 +0200

    create GlobalStyles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import styled from 'styled-components';
+import GlobalStyles from './styles/GlobalStyles';
+
+const H1 = styled.h1`
+  font-size: 48px;
+  font-weight: 600;
+`;
+
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <GlobalStyles />
+      <div>
+        <H1>Hello Kris</H1>
+      </div>
+    </>
+  );
 }
 
 export default App;
