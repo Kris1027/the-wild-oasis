79a2e4b7adbc664466e902aed74f6020b024c979
Kris1027 <obarzanek.work@gmail.com>
2023-09-25 22:49:59 +0200

    connecting Supabase

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://zljskoldukfacquejluf.supabase.co';
+const supabaseKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpsanNrb2xkdWtmYWNxdWVqbHVmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTU2MzEyMTYsImV4cCI6MjAxMTIwNzIxNn0.RCKn2JbpNVtTvU9forOuqfayilwf7spbx_XSjKBvPyQ';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
